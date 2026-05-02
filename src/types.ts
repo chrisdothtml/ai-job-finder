12490afd54185f88ccd8c3fa0b25c8f9ab56388c
Chris Deacy <chrisdothtml@gmail.com>
2026-05-01 17:43:08 -0700

    rm unused file

--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export type AnyFn = (...args: any[]) => any;
-export type AnyAsyncFn = (...args: any[]) => Promise<any>;
-export type DefaultExport<E> = { default: E };
-export type Flatten<T> = { [K in keyof T]: T[K] };
-export type ValueOf<T> = T[keyof T];
